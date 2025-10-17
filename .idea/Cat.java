fc6e18a376f4dc1c0c4570364142f645710ac03c
nikeronin <eronin.crm@gmail.com>
2025-10-17 14:31:52 +0500

    third task

---
+++ .idea/Cat.java
@@ -0,0 +1,9 @@
+package com.-java;
+
+    public class Cat {
+        public float catSpeed = 10.5;
+        public int catWeight = 6;
+        static String catColour = "Grey";
+
+        public System.out.print("Цвет кота: " + catColour);
+    }
---
+++ .idea/Main.java
@@ -0,0 +1,11 @@
+package com.-java;
+
+    public class Main {
+
+        public static void main(String[] args) {
+             Cat basik = new Cat();
+             basik.catSpeed = 8.5;
+             basik.catWeight = 7;
+             basik.catColour = "White";
+        }
+}
